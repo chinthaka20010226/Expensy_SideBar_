06f86ad9a9f06c7f075b78067de73f99a674c9a8
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-02 20:50:20 +0530

    setup the Images...

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import IMAGES from '../Assets/index';
+import { FaBars } from 'react-icons/fa'
+
+import '../Style/SideBar.css';
 
 function SideBar(){
     return(
@@ -6,9 +10,15 @@ function SideBar(){
             <div className='side-bar'>
                 <div className='top-section'>
                     <h1 className='logo'>Logo</h1>
-                    <div className='bar'></div>
+                    <div className='bar'>
+                        <FaBars />
+                    </div>
                 </div>
-                <div className='body-section'></div>
+                <div className='body-section'>
+                    <div>
+                        <img src={IMAGES.setting_icon} alt=''></img>
+                    </div>
+                </div>
             </div>
             <main></main>
         </div>
